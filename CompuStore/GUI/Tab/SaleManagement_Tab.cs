56cc6280cc157a7c7f5c34941a46f26160b7767a
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2022-11-12 17:05:31 +0700

    đm

--- CompuStore/GUI/Tab/SaleManagement_Tab.cs
+++ CompuStore/GUI/Tab/SaleManagement_Tab.cs
@@ -15,7 +15,7 @@ using System.Windows.Forms;
 
 namespace CompuStore.Tab
 {
-    public partial class SaleManagement_Tab : BaseTab
+    public partial class SaleManagement_Tab : Warehouse_UC
     {
         public static string nameIdCommonSpecs;
         public static Dictionary<COMMON_SPECS, int> listProduct = new Dictionary<COMMON_SPECS, int>();
